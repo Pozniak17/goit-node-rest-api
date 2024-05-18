8dd278fde08dc5c8dc8dab8b8b5420ce492714d9
Yevhenii Pozniak <jekamanu@gmail.com>
2024-05-18 15:53:38 +0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import "dotenv/config";
 import "./db.js";
 import contactsRouter from "./routes/contactsRouter.js";
 
@@ -15,10 +15,12 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
+// Handle 404 Error
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
+// Handle 500 Error
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
