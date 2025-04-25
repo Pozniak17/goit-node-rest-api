ea383a4caaf5463f72c408841374e074d6d3923d
Yevhenii Pozniak <jekamanu@gmail.com>
2025-04-25 16:07:54 +0300

    update

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,5 +1,9 @@
 import HttpError from "../helpers/HttpError.js";
 import contactsService from "../services/contactsServices.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
 export const getAllContacts = async (req, res) => {
   const data = await contactsService.listContacts();
@@ -22,8 +26,67 @@ export const getOneContact = async (req, res, next) => {
   }
 };
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
 
-export const createContact = (req, res) => {};
+  try {
+    const data = await contactsService.removeContact(id);
 
-export const updateContact = (req, res) => {};
+    if (!data) {
+      throw HttpError(404);
+    }
+
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  const newData = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+
+  const { error, value } = createContactSchema.validate(newData);
+
+  try {
+    const contact = await contactsService.addContact(value);
+    if (error) {
+      res.status(400).json({ message: error.message });
+    }
+
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  const { id } = req.params;
+
+  const updatedData = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+
+  const { error, value } = updateContactSchema.validate(updatedData);
+
+  try {
+    const contact = await contactsService.updateContact(id, value);
+    // помилка при не проходженні валідації
+    if (error) {
+      res.status(400).json({ message: error.message });
+    }
+
+    if (!contact) {
+      throw HttpError(404);
+    }
+
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,15 @@
 import Joi from "joi";
 
-export const createContactSchema = Joi.object({
+// {name, email, phone}
 
-})
+export const createContactSchema = Joi.object({
+  name: Joi.string().required().min(5).max(20),
+  email: Joi.string().required().min(5),
+  phone: Joi.string().required().min(5),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  name: Joi.string().min(5).max(20),
+  email: Joi.string().min(5),
+  phone: Joi.string().min(5),
+});
